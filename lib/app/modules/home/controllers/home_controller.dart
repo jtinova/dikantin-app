f6723ad88faed6baa2c1445d9b6818bde563f7f3
HiragiIsMe <hiragii421@gmail.com>
2024-10-01 13:27:02 +0700

    fix bug order

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -13,6 +13,7 @@ import '../../../data/models/waktu_model.dart';
 import '../../../data/providers/customer_provider.dart';
 import '../../../data/providers/menu_provider.dart';
 import '../../../data/providers/services.dart';
+import '../../order/controllers/order_controller.dart';
 
 class HomeController extends GetxController with GetTickerProviderStateMixin {
   final TextEditingController catatanController = TextEditingController();
@@ -56,7 +57,7 @@ class HomeController extends GetxController with GetTickerProviderStateMixin {
       total += itemTotal;
     }
     // Menambahkan biaya kirim ke total harga
-    final biayakirim = biayaData.value.data ?? 0;
+    final biayakirim = OrderController().calculateValueBasedOnRange() ?? 0;
     total += biayakirim;
     return total;
   }
