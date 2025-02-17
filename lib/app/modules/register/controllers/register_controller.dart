108050ef6250df08aca7c2e3c27bee83d29f9b9d
Alzando <123951867+allzndd@users.noreply.github.com>
2025-02-17 12:01:46 +0700

    Update register_controller.dart

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,6 +1,7 @@
 import 'package:dikantin/app/data/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class RegisterController extends GetxController {
   final RegisterProvider registerProvider = RegisterProvider();
@@ -10,8 +11,10 @@ class RegisterController extends GetxController {
   TextEditingController phoneController = TextEditingController();
   TextEditingController addressController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
   final RxBool obscureText = true.obs;
   final RxBool isLoading = false.obs;
+  final RxBool obscureConfirmText = true.obs;
 
   final count = 0.obs;
   @override
@@ -33,5 +36,18 @@ class RegisterController extends GetxController {
 
   void toggleObscureText() {
     obscureText.value = !obscureText.value;
+  }
+
+  void toggleObscureConfirmText() {
+    obscureConfirmText.value = !obscureConfirmText.value;
+  }
+
+  void register() {
+    if (passwordController.text != confirmPasswordController.text) {
+      EasyLoading.showError('Password tidak sama');
+      return;
+    }
+
+    // Proceed with registration
   }
 }
