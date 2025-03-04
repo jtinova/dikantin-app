eb60329d5929af7c7993db2f5d3b7a235a4ef55c
hairulanaam <hairulanam602@gmail.com>
2025-03-04 12:22:34 +0700

    Resolve merge conflict

--- lib/app/modules/send_email/controllers/send_email_controller.dart
+++ lib/app/modules/send_email/controllers/send_email_controller.dart
@@ -4,20 +4,8 @@ class SendEmailController extends GetxController {
   //TODO: Implement SendEmailController
 
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
 
   void increment() => count.value++;
 }
